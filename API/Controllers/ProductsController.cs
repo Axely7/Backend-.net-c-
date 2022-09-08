4dfbeb4b2d0ee4142ecdff75fdbcbcbd785d3a6c
Axely7 <axely7@hotmail.com>
2022-09-08 14:26:15 -0500

    Methods api CRUD

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,6 +34,41 @@ namespace API.Controllers
             return await context.Products.FindAsync(id);
         }
 
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+            context.Add(product);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Products.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound();
+
+            context.Remove(new Product() { Id = id });
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(Product product, int id)
+        {
+            if (product.Id != id)
+                return BadRequest("El id del auto no coincide con el id de la URL");
+
+            var existe = await context.Products.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound();
+
+            context.Update(product);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
 
     }
 }
